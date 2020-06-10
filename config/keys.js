0dd17334c751177a5d664fe865caa7c5b838bf5f
Abimbola Balogun <olorunyomi97@gmail.com>
2020-06-10 02:32:11 +0100

    deleted

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://Biggie:soodamnawesome@biggiecluster-y5uge.mongodb.net/test?retryWrites=true&w=majority',
-    secretOrKey: 'secret'
-};
--- server.js
+++ server.js
@@ -1,17 +1,7 @@
 var PORT = process.env.PORT || 5500;
 const express = require('express');
-const mongoose = require('mongoose');
-
 var app = express();
 
-// DB Config
-const db = require('./config/keys').mongoURI;
-
-// connect to mongoDB through mongoose //
-mongoose
-    .connect(db)
-    .then(() => console.log('MongoDB Connected'))
-    .catch(err => console.log(err));
 
     
 const port = process.env.PORT || 5500;
